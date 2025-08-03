2db6ce8161c0074b9e3e60fb95d5a21c251f34ac
Mia-HaGyeong <miahg929@gmail.com>
2025-08-04 02:42:19 +0900

    Move project to day_1 folder and update GitHub Pages config
    - 프로젝트를 day_1 폴더로 이동
    - vite.config.js base 경로를 /hmac/day_1/로 수정
    - GitHub Actions 워크플로우를 day_1 폴더용으로 업데이트
    - 배포 URL: https://congrats-hg.github.io/hmac/day_1/

--- day_1/vite.config.js
+++ day_1/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/hmac/', // GitHub 저장소 이름으로 변경하세요
+  base: '/hmac/day_1/', // GitHub 저장소 이름으로 변경하세요
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
